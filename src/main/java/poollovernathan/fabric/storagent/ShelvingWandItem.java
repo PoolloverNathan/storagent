15b4e2f95b6e86a9b43f3dcfbba67af9bf3fb630
PoolloverNathan <nathan.kulzer@protonmail.com>
2023-08-16 21:45:30 -0400

    Remove emptied stacks from wand inventory

--- src/main/java/poollovernathan/fabric/storagent/ShelvingWandItem.java
+++ src/main/java/poollovernathan/fabric/storagent/ShelvingWandItem.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -43,12 +44,13 @@ public class ShelvingWandItem extends BundleItem {
         }
         var block = block(context.getStack(), ShelfSurfaceMaterial.OAK, ShelfSupportMaterial.OAK, ShelfHeight.MEDIUM);
         var creative = Optional.ofNullable(context.getPlayer()).map(PlayerEntity::isCreative).orElse(false);
+        NbtList items = null;
         NbtCompound surfaceStack = null;
         NbtCompound supportStack = null;
         if (!creative) {
             var nbt = context.getStack().getNbt();
             if (nbt == null) return ActionResult.FAIL;
-            var items = nbt.getList("Items", NbtElement.COMPOUND_TYPE);
+            items = nbt.getList("Items", NbtElement.COMPOUND_TYPE);
             for (var stackData : items) {
                 var stack = ItemStack.fromNbt((NbtCompound) stackData);
                 if (surfaceStack == null && stack.getCount() >= 2 && stack.getItem() == block.surface.slab.asItem()) {
@@ -64,7 +66,17 @@ public class ShelvingWandItem extends BundleItem {
         if (nestedResult.isAccepted()) {
             if (!creative) {
                 surfaceStack.putInt("Count", surfaceStack.getInt("Count") - 2);
+                if (surfaceStack.getInt("Count") <= 0) {
+                    if (!items.remove(surfaceStack)) {
+                        throw new IllegalStateException("Surface stack does not exist in the item, despite being copied from the item list - this should never happen");
+                    }
+                }
                 supportStack.putInt("Count", supportStack.getInt("Count") - 2);
+                if (supportStack.getInt("Count") <= 0) {
+                    if (!items.remove(supportStack)) {
+                        throw new IllegalStateException("Support stack does not exist in the item, despite being copied from the item list - this should never happen");
+                    }
+                }
                 context.getStack().increment(1); // workaround for BlockItem consuming the item
             }
             return ActionResult.SUCCESS;
